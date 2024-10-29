2a7b0e005346ee283af3fda4ee9a5710d865b82f
Quinten Aspeslagh <quinten.aspeslagh@student.howest.be>
2024-10-29 21:16:58 +0100

    Add live stats

---
+++ VolleyballMatch.Core/Entities/PlayerLiveStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolleyballMatch.Core.Entities
+{
+    public class PlayerLiveStats
+    {
+        public string PlayerName { get; set; }
+        public int AttackPointAmount { get; set; }
+        public int BlockPointAmount { get; set; }
+        public int PointAmount { get; set; }
+        public int AceAmount { get; set; }
+        public int ReceptionfaultAmount { get; set; }
+        public int FaultAmount { get; set; }
+    }
+}
